0be30abd250baffea9de2fbb2e5a8f794467298f
farouk-10 <108435779+farouk-10@users.noreply.github.com>
2022-07-07 15:54:28 +0100

    Add files via upload

---
+++ farouk touhafi.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **TOUHAFI**
+# an entrepreneur
+
+# *I'm interested in data science because I'm an engineer in Finance and I want to build a business and I want to use AI in my analysis for my businesses also I'm intersted in building a cloud like IBM's in my country Morocco*
+
+# ### I will do a simple calculation of 1 - 1 = ?
+
+# In[3]:
+
+
+1-1
+
+
+# [IBM home page morocco](https://www.ibm.com/ma-fr)
+# 1. finance
+#    1. stochastic
+#    2. rick theorie
+# 2. alphabet 
+#      1. A
+#      2. B
+# - subjects:
+#   * maths
+#   * science
+#   * english
+# |ID|name|age|
+# |--|----|---|
+# |01|amour|18|
+# |02|lala|19|
+
+# In[ ]:
+
+
+
+
